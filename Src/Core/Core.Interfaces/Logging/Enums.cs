b9310ea212e6a50b0e6844cd5fbf31893236169c
Arby <Arby@YAYSOFT.local>
2016-01-06 17:06:32 -0600

    User extensible enum for severity.

--- Src/Core/Core.Interfaces/Logging/Enums.cs
+++ Src/Core/Core.Interfaces/Logging/Enums.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
 
 namespace Core.Interfaces.Logging
 {
-    public enum LogMessageSeverity
+    public class UserExtensibleEnum
     {
-        Information,
-        Warning,
-        Error,
-        Critical
+        public int Code { get; private set; }
+        public string Name { get; private set; }
+
+        protected UserExtensibleEnum(int code, [CallerMemberName] string name = "")
+        {
+            Code = code;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static bool operator >(UserExtensibleEnum e1, UserExtensibleEnum e2)
+        {
+            return e1.Code > e2.Code;
+        }
+        public static bool operator <(UserExtensibleEnum e1, UserExtensibleEnum e2)
+        {
+            return e1.Code < e2.Code;
+        }
+
+
+    }
+
+    public class LogMessageSeverity : UserExtensibleEnum
+    {
+        public static LogMessageSeverity Information = new LogMessageSeverity(100);
+        public static LogMessageSeverity Warning =     new LogMessageSeverity(400);
+        public static LogMessageSeverity Error =       new LogMessageSeverity(1600);
+        public static LogMessageSeverity Critical =    new LogMessageSeverity(6400);
+
+        public LogMessageSeverity(int code, [CallerMemberName] string name = "") : base(code,name) { }
     }
 }
--- Src/Test/UnitTests/Core.Logging.Test/UnitTest1.cs
+++ Src/Test/UnitTests/Core.Logging.Test/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core.Logging;
+using Core.Interfaces.Logging;
+using System.Runtime.CompilerServices;
 
 namespace Core.Logging.Test
 {
@@ -10,5 +13,34 @@ namespace Core.Logging.Test
         public void TestMethod1()
         {
         }
+
+        [TestMethod]
+        public void TestUserExtensibleEnum()
+        {
+            Assert.AreEqual(6400, LogMessageSeverity.Critical.Code, "Default Code Resolves Correctly");
+            Assert.AreEqual("Critical", LogMessageSeverity.Critical.ToString(), "Default Name Resolves Correctly");
+            Assert.AreEqual(999, CustomLogMessageSeverity.George.Code, "Custom Code Resolves Correctly");
+            Assert.AreEqual("George", CustomLogMessageSeverity.George.ToString(), "Custom Name Resolves Correctly");
+
+            Assert.IsTrue(LogMessageSeverity.Critical > CustomLogMessageSeverity.George, "Can Compare");
+
+            // :(
+
+            //LogMessageSeverity val = CustomLogMessageSeverity.George;
+            //switch (val)
+            //{
+            //    case LogMessageSeverity.Critical.Code: break;
+            //    case CustomLogMessageSeverity.George: break;
+            //    default: Assert.Fail("This shouldn't happen."); break;
+            //}
+        }
     }
+
+    public class CustomLogMessageSeverity : LogMessageSeverity
+    {
+        public static LogMessageSeverity George = new LogMessageSeverity(999);
+
+        public CustomLogMessageSeverity(int code, [CallerMemberName] string name = "") : base(code,name) { }
+    }
+
 }
