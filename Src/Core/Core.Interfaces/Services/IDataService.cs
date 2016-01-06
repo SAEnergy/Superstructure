2b9883c45989fc7cfa1441260dc845b26c9acfdb
Bobby Merkin <bobby.merkin@saenergy.com>
2016-01-06 12:26:33 -0600

    Add basic implementation of dataservice, need to make it use configuration engine in future, add models project and one model to be used by dataservice

--- Src/Core/Core.Interfaces/Services/IDataService.cs
+++ Src/Core/Core.Interfaces/Services/IDataService.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Interfaces.Services
 {
     public interface IDataService
     {
+        bool Delete<T>(T obj) where T : class;
+
+        bool Delete<T>(Func<T, bool> where) where T : class;
+
+        bool Update<T>(int key, T obj) where T : class;
+
+        bool Insert<T>(T obj) where T : class;
+
+        List<T> Find<T>(Func<T, bool> where) where T : class;
+
+        T Find<T>(int key) where T : class;
     }
 }
--- Src/Test/Test.Mocks/DataServiceMock.cs
+++ Src/Test/Test.Mocks/DataServiceMock.cs
@@ -9,5 +9,34 @@ namespace Test.Mocks
 {
     public class DataServiceMock : IDataService
     {
+        public bool Delete<T>(Func<T, bool> where) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete<T>(T obj) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find<T>(int key) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Find<T>(Func<T, bool> where) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert<T>(T obj) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update<T>(int key, T obj) where T : class
+        {
+            throw new NotImplementedException();
+        }
     }
 }
