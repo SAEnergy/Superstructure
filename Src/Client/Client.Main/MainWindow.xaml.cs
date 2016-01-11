9ea34b0ed928beb97bf35f4434910ae56684f71e
Robert Bean <robert.bean@saenergy.com>
2016-01-11 09:57:02 -0600

    Initial client framework commit.

---
+++ Src/Client/Client.Main/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Client.Base;
+
+namespace Client.Main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : GlassWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
