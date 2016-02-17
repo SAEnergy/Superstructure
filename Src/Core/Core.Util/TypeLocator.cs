0537ceb243e94fbb3ec8377b23a07139a62fc786
Robert Bean <robert.bean@saenergy.com>
2016-02-16 21:38:20 -0600

    Type locator now uses AppDomain so files are not locked.

--- Src/Core/Core.Util/TypeLocator.cs
+++ Src/Core/Core.Util/TypeLocator.cs
@@ -12,19 +12,77 @@ namespace Core.Util
     {
         public static IEnumerable<Type> FindTypes(string searchPattern, Type supportedInterface)
         {
-            //todo: this should do the search in an appdomain, then destroy it, so files are not locked.
+            string baseDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
             List<Type> found = new List<Type>();
 
-            var files = Directory.GetFiles(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), searchPattern, SearchOption.TopDirectoryOnly);
+            AppDomain tempDomain = AppDomain.CreateDomain("bob");
 
-            foreach (string file in files)
+            TypeLocatorWorker bob = (TypeLocatorWorker)tempDomain.CreateInstanceAndUnwrap(Assembly.GetExecutingAssembly().ToString(), typeof(TypeLocatorWorker).FullName);
+
+            string[] foundTypes = null;
+            string[] errors = null;
+
+            bob.Search(new string[] { supportedInterface.AssemblyQualifiedName }, baseDir, searchPattern, out foundTypes, out errors);
+
+            foreach (string s in errors)
             {
-                var assm = Assembly.LoadFile(file);
-                var types = assm.GetTypes().Where(t => supportedInterface.IsAssignableFrom(t) && t.ContainsGenericParameters == false);
-                found.AddRange(types);
+                // todo: get this back to logging system?
             }
 
+            foreach (string s in foundTypes)
+            {
+                found.Add(Type.GetType(s));
+            }
+
+            AppDomain.Unload(tempDomain);
+
             return found;
+        }
+    }
+
+    public class TypeLocatorWorker : MarshalByRefObject
+    {
+        public void Search(string[] typeNames, string baseDirectory, string searchPattern, out string[] foundTypes, out string[] errors)
+        {
+            List<string> toRet = new List<string>();
+            List<string> errRet = new List<string>();
+            List<Type> types = new List<Type>();
+
+            foreach (string s in typeNames)
+            {
+                types.Add(Type.GetType(s));
+            }
+
+            try
+            {
+                var files = Directory.GetFiles(baseDirectory, searchPattern, SearchOption.TopDirectoryOnly);
+
+                foreach (string file in files)
+                {
+                    try
+                    {
+                        var assm = Assembly.LoadFile(file);
+
+                        foreach (Type type in types)
+                        {
+                            var found = assm.GetTypes().Where(t => t != type && type.IsAssignableFrom(t) && t.ContainsGenericParameters == false);
+                            toRet.AddRange(found.Select(t => t.AssemblyQualifiedName));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        errRet.Add(ex.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errRet.Add(ex.ToString());
+            }
+
+            foundTypes = toRet.ToArray();
+            errors = errRet.ToArray();
         }
     }
 }
