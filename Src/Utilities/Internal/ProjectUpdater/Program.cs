11222054f7f969eac0cf49e582a75cb939cec867
Bobby Merkin <bobby.merkin@saenergy.com>
2016-02-19 09:50:17 -0600

    Update ProjectUpdater to set the output directories if needed.  update VersionReader and ProjectUpdater with results

--- Src/Utilities/Internal/ProjectUpdater/Program.cs
+++ Src/Utilities/Internal/ProjectUpdater/Program.cs
@@ -2,6 +2,7 @@
 using Core.Logging;
 using Core.Logging.LogDestinations;
 using Microsoft.Build;
+using Microsoft.Build.Construction;
 using Microsoft.Build.Evaluation;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,15 @@ namespace ProjectUpdater
         private const string _searchPath = "*.csproj";
         private const string _globalAssemblyInfo = "GlobalAssemblyInfo.cs";
         private static int _verifyResult;
+        private const string _defaultFolderPath = "Run";
+        private const string _folderPathFormat = "$(SolutionDir)Out\\{0}\\$(Configuration)";
+
+        private static Dictionary<string, string> _folderMap = new Dictionary<string, string>()
+        {
+            { "Test", "Test"},
+            { "Utilities", "Util" },
+            { "Installer", "Installer" }
+        };
 
         public static int Main(string[] args)
         {
@@ -41,7 +51,7 @@ namespace ProjectUpdater
 
                         var project = new Project(projectFile);
 
-                        GlobalAssemblyUpdater(project, projectFile);
+                        GlobalAssemblyUpdater(project);
 
                         OutFolderUpdater(project);
                     }
@@ -55,7 +65,7 @@ namespace ProjectUpdater
             return _verifyResult;
         }
 
-        private static void GlobalAssemblyUpdater(Project project, string projectFile)
+        private static void GlobalAssemblyUpdater(Project project)
         {
             if (project != null)
             {
@@ -65,33 +75,61 @@ namespace ProjectUpdater
                 {
                     if (!_settings.IsReadOnly)
                     {
-                        _logger.Log(string.Format("Adding \"{0}\" link to project \"{1}\"", _globalAssemblyInfo, projectFile));
+                        _logger.Log(string.Format("Adding \"{0}\" link to project \"{1}\"", _globalAssemblyInfo, project.FullPath));
 
                         var kvp = new KeyValuePair<string, string>("Link", string.Concat("Properties\\", _globalAssemblyInfo));
 
-                        var rc = project.AddItem("Compile", string.Concat(FindRelativePathFromFolder(projectFile), _globalAssemblyInfo), new[] { kvp });
+                        var rc = project.AddItem("Compile", string.Concat(FindRelativePathFromFolder(project.FullPath), _globalAssemblyInfo), new[] { kvp });
 
                         project.Save();
                     }
 
                     if (_settings.Verify)
                     {
-                        _logger.Log(string.Format("Project \"{0}\" did not include link to \"{1}\"", projectFile, _globalAssemblyInfo));
+                        _logger.Log(string.Format("Project \"{0}\" did not include link to \"{1}\"", project.FullPath, _globalAssemblyInfo));
                         _verifyResult--;
                     }
                 }
                 else
                 {
-                    _logger.Log(string.Format("Project \"{0}\" already has a link to \"{1}\"", projectFile, _globalAssemblyInfo));
+                    _logger.Log(string.Format("Project \"{0}\" already has a link to \"{1}\"", project.FullPath, _globalAssemblyInfo));
                 }
             }
         }
 
         private static void OutFolderUpdater(Project project)
         {
-            if(project != null)
+            if (project != null)
             {
-                
+                foreach(var child in project.Xml.AllChildren.Where(c => c is ProjectPropertyElement))
+                {
+                    var element = child as ProjectPropertyElement;
+
+                    if(element != null)
+                    {
+                        if(element.Name == "OutputPath")
+                        {
+                            var path = string.Format(_folderPathFormat, GetPathMap(project.FullPath));
+
+                            if (element.Value != path)
+                            {
+                                if (!_settings.IsReadOnly)
+                                {
+                                    _logger.Log(string.Format("Setting project \"{0}\" property \"OutputPath\" value  to \"{1}\"", project.FullPath, path));
+
+                                    element.Value = path;
+                                    project.Save();
+                                }
+
+                                if(_settings.Verify)
+                                {
+                                    _logger.Log(string.Format("Project \"{0}\" does not have property \"OutputPath\" set to value \"{1}\"", project.FullPath, path));
+                                    _verifyResult--;
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
 
@@ -116,6 +154,36 @@ namespace ProjectUpdater
             else
             {
                 throw new NotSupportedException();
+            }
+
+            return retVal;
+        }
+
+        private static string GetPathMap(string path)
+        {
+            var retVal = _defaultFolderPath;
+
+            int index = path.IndexOf(_settings.SourceFolder);
+
+            if(index > -1)
+            {
+                var substring = path.Substring(index + _settings.SourceFolder.Length + 1);
+
+                var split = substring.Split(new char[] { Path.DirectorySeparatorChar });
+
+                if(split.Length > 1)
+                {
+                    string val;
+
+                    if(_folderMap.TryGetValue(split[0], out val))
+                    {
+                        retVal = val;
+                    }
+                }
+            }
+            else
+            {
+                throw new NotSupportedException(); 
             }
 
             return retVal;
