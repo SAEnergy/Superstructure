93174d97c68251feb075fec1a7b8b780b8801b27
Bobby Merkin <bobby.merkin@saenergy.com>
2016-02-22 13:53:15 -0600

    Add some bits to UnitTestJob to assist in unit testing, maybe.  Update unit test to prove Schedulers plugin system is working.

--- Src/Test/UnitTests/Core.Scheduler.Test/SchedulerTest.cs
+++ Src/Test/UnitTests/Core.Scheduler.Test/SchedulerTest.cs
@@ -2,18 +2,48 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Test.Mocks;
 using Core.Models.Persistent;
+using Test.Helpers;
+using Test.Plugins.Mocks;
+using System.Threading;
+using System.Linq;
 
 namespace Core.Scheduler.Test
 {
     [TestClass]
     public class SchedulerTest
     {
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            SingletonHelper.Clean(typeof(SchedulerComponent));
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            SingletonHelper.Clean(typeof(SchedulerComponent));
+        }
+
         [TestMethod]
         public void SchedulerTest_AddCustomJob()
         {
             var scheduler = SchedulerComponent.CreateInstance(new LoggerMock(), new DataComponentMock());
 
+            scheduler.Start();
+
             scheduler.AddJob(BuildMeAJob());
+
+            //give the scheduler time to create the instance and run it
+            while(UnitTestJob.Instances == null)
+            {
+                Thread.Sleep(100);
+            }
+
+            var job = UnitTestJob.Instances.FirstOrDefault();
+
+            Assert.IsNotNull(job);
+
+            scheduler.Stop();
         }
 
         private JobConfiguration BuildMeAJob()
