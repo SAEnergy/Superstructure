429026f6bcbf6ce4bbe003f6640d8f86773b47af
Bobby Merkin <bobby.merkin@saenergy.com>
2016-03-08 12:04:05 -0600

    Add a first draft of the LogViewerView, primarily to test RemoteLogViewerHost

---
+++ Src/Client/Client.Controls/ViewModels/LogViewModel.cs
@@ -0,0 +1,92 @@
+﻿using Client.Base;
+using Core.Comm;
+using Core.Interfaces.Components.Logging;
+using Core.Interfaces.ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client.Controls
+{
+    public class LogViewModel : ViewModelBase<IRemoteLogViewer>, IRemoteLogViewerCallback
+    {
+        public bool ViewDetail { get; set; }
+
+        public Visibility IsVisible
+        {
+            get
+            {
+                return ViewDetail ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        public int MaxMessages { get; set; }
+
+        //public SimpleCommand TogglePause { get; set; }
+
+        public ObservableCollection<LogMessage> LogMessages { get; private set; }
+
+        public LogViewModel(ViewBase parent) : base(parent)
+        {
+            LogMessages = new ObservableCollection<LogMessage>();
+
+            LogMessages.Add(new LogMessage() { Severity = LogMessageSeverity.Information, Message = "Connecting to server..." });
+        }
+
+        protected override void OnConnect(ISubscription source)
+        {
+            try
+            {
+                this.BeginInvoke(() =>
+                {
+                    LogMessages.Add(new LogMessage() { Severity = LogMessageSeverity.Information, Message = "Connected." });
+                });
+
+                Channel.Register();
+            }
+            catch (Exception)
+            {
+            }
+
+            base.OnConnect(source);
+        }
+
+        protected override void OnDisconnect(ISubscription source, Exception error)
+        {
+            this.BeginInvoke(() =>
+            {
+                LogMessages.Add(new LogMessage() { Severity = LogMessageSeverity.Information, Message = "Connection terminated." });
+            });
+
+            base.OnDisconnect(source, error);
+        }
+
+        public override void Dispose()
+        {
+            try
+            {
+                Channel.Unregister();
+            }
+            catch (Exception)
+            {
+            }
+
+            base.Dispose();
+        }
+
+        public void ReportMessages(LogMessage[] messages)
+        {
+            this.BeginInvoke(() =>
+            {
+                foreach(var message in messages)
+                {
+                    LogMessages.Add(message);
+                }
+            });
+        }
+    }
+}
---
+++ Src/Client/Client.Plugins.Test/Views/TestLogViewPanel.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Client.Base;
+using System.Threading;
+
+namespace Client.Plugins.Test
+{
+    /// <summary>
+    /// Interaction logic for TestLogViewPanel.xaml
+    /// </summary>
+    [PanelMetadata(DisplayName = "Log View Test", IconPath = "images/warning.png")]
+    public partial class TestLogViewPanel : PanelBase
+    {
+        public TestLogViewPanel()
+        {
+            InitializeComponent();
+        }
+
+        public override void Dispose()
+        {
+            logView.Dispose();
+
+            base.Dispose();
+        }
+    }
+}
