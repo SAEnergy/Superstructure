66640885945c4a644e238524d36e816849ad8568
Kris Hayes <kris.hayes@saenergy.com>
2016-03-29 23:00:45 -0500

    Adds a start of the filtering framework.

---
+++ Src/Components/Logging.Component/LogMessageFilters/Filter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces.Components.Logging;
+
+namespace Core.Logging.LogMessageFilters
+{
+    class Filter : IFilter
+    {
+        LogMessage IFilter.Filter(LogMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/Components/Logging.Component/LogMessageFilters/LogMessageFilterBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces.Components.Logging;
+
+namespace Core.Logging.LogMessageFilters
+{
+    public abstract class LogMessageFilterBase : List<IFilter>, ILogMessageFilter
+    {
+        public abstract LogMessage Filter(LogMessage message);
+
+        public abstract void FilterAdd(IFilter filter);
+
+        public abstract void FilterClear();
+    }
+}
